9f2e35828008706e80f5ab377aab2c6ffded164f
AM_PC <ashakhtorin@inbox.ru>
2018-12-04 21:50:25 +0300

    styles again

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,10 +9,24 @@ import { CartPriceService } from './../shared/cart-price.service';
 })
 export class HeaderComponent implements OnInit {
 	currentPrice: string;
+
   constructor(private priceService: CartPriceService) { }
 
   ngOnInit() {
   	this.currentPrice = this.priceService.price;
   }
 
+}
+function headerFunc() {
+	let header = document.getElementById('header');
+	let currentPosition = header.offsetTop;
+	if (window.pageYOffset > currentPosition) {
+    	header.classList.add("sticky");
+  	} 
+  	else {
+    	header.classList.remove("sticky");
+  	}
+}
+window.onscroll = () => {
+  	headerFunc();
 }
