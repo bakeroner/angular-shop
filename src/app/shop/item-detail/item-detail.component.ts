9ad0dc87575cab63936cbbac5668ea779bb2f079
AM_PC <ashakhtorin@inbox.ru>
2018-12-05 15:11:03 +0300

    unsubscribe on destroy added

--- src/app/shop/item-detail/item-detail.component.ts
+++ src/app/shop/item-detail/item-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
@@ -12,13 +12,14 @@ import {Beer} from './../../shared/beer';
 })
 export class ItemDetailComponent implements OnInit {
 	currentItem: Beer;
+	subscription: any;
 	currentId: any = +this.route.snapshot.paramMap.get('id');
 	goBack(): void {
 		this.router.navigate(["../"], {relativeTo: this.route});
 	}
 	getItem(): void {
 		if (this.currentId) {
-			this.dbMeth.getElement(this.currentId).subscribe(
+			this.subscription = this.dbMeth.getElement(this.currentId).subscribe(
 				result => {this.currentItem = result},
 				error => {console.log(error)}
 			);
@@ -33,6 +34,10 @@ export class ItemDetailComponent implements OnInit {
   	if (this.currentId) {
   		this.getItem();
   	}
+  }
+  ngOnDestroy() {
+  	console.log('destroyed');
+  	this.subscription.unsubscribe();
   }
 
 }
--- src/app/shop/search/search.component.ts
+++ src/app/shop/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router';
@@ -15,6 +15,8 @@ import { DbMethodsService } from './../../shared/db-methods.service';
 })
 export class SearchComponent implements OnInit {
 	searchForm: FormGroup;
+  listSubscription: any;
+  valueSubscription: any;
 	filters: string[] = ['All'];
   onValueChange(value: any): void {
     this.router.navigate(["/shop", {category: value.category, name: value.beerName}], {relativeTo: this.route});
@@ -25,7 +27,7 @@ export class SearchComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private dbMeth: DbMethodsService, private http: Http) { }
 
   ngOnInit() {
-    this.dbMeth.getType().subscribe(
+    this.listSubscription = this.dbMeth.getType().subscribe(
   		  result => {this.filters = this.filters.concat(result);},
   		  error => {console.log(error)}
   	);
@@ -33,8 +35,13 @@ export class SearchComponent implements OnInit {
   		category: new FormControl("All"),
   		beerName: new FormControl('')
   	});
-    this.searchForm.valueChanges.pipe(debounceTime(1000))
+    this.valueSubscription = this.searchForm.valueChanges.pipe(debounceTime(500))
         .subscribe(data => this.onValueChange(data));
     }
+  ngOnDestroy() {
+    console.log('destroyed');
+    this.listSubscription.unsubscribe();
+    this.valueSubscription.unsubscribe();
+  }
 
 }
